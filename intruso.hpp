b0a60b41ab6c972d142319c269007e25344e352c
Pedro-Boseja <104467351+Pedro-Boseja@users.noreply.github.com>
2022-11-01 13:06:25 -0300

    Create intruso.hpp

---
+++ intruso.hpp
@@ -0,0 +1,18 @@
+#ifndef INTRUSO_HPP
+#define INTRUSO_HPP
+
+#include <string>
+#include <vector>
+#include <map>
+
+class Intruso {
+    private:
+        std::vector<std::map<char, int*>> _vazou_chave;
+        std::vector<std::string> _vazou_senha;
+
+    public:
+        void set_senha_vazada(std::string vazou);
+        std::string crack_senha();
+};
+
+#endif
